8db56767e5514a014ce9a50eda53faf36c661cb6
Deepak Karishetti <r10.deepak@gmail.com>
2019-07-22 02:41:10 -0600

    modified

--- binary_search.cpp
+++ binary_search.cpp
@@ -1,46 +1,45 @@
+// Simple program implementation of binary search algorithm in c++ and is applicable only on SORTED lists
+
 #include <iostream>
 
 using namespace std;
 
-int binary_search(int arr[], int left, int right, int val)
+// function to compute the binary search
+int binary_search(int arr[], int size, int val)
 {
-	while (right >= left)
+	int low = 0;
+	int high = size - 1;	
+	int mid;
+	// runs until the end condition is met like recursively
+	while (low <= high)
 	{
-		int mid = left + (right - left) / 2;
-
-		// if val present at mid
-		if (arr[mid] == val)
+		mid = (low + high) / 2;
+	
+		if (val == arr[mid])
 		{
 			return mid;
-		}	
-		// if val smaller than mid then has to be on the left
-		else if (arr[mid] < val)
-		{
-			return binary_search(arr, left, mid - 1, val);
 		}
-		// else has to be on right
+		else if (val > arr[mid])
+		{
+			low = mid + 1;
+		}
 		else
 		{
-		return binary_search(arr, mid+1, right, val);
+			high = mid - 1;	
 		}
 	}
-	// if val not present in the given array
 	return -1;
-}
+}	
 
 int main()
 {
 	int arr[] = {2, 4, 6, 8, 10, 12, 14, 16, 18, 20};
 	int x;
-	for (i : arr)
-	{
-		cout << i << " ";
-	}
-	cout << "\n";
+	
 	cout << "Enter the number to be found: " << "\n";
 	int n = sizeof(arr) / sizeof(arr[0]); 
 	cin >> x;
-	int result = binary_search(arr, 0, n - 1, x);
+	int result = binary_search(arr, n, x);
 	if (result == -1)
 	{	
 		cout << "Element entered is not present in the array!" << "\n";
@@ -49,6 +48,7 @@ int main()
 	{
 		cout << "Element is present at index: " << result << "\n";
 	}
+
 	return 0;
 }
 				
