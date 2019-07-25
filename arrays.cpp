b573201122f543febdbffa33c93d22082d8d6321
Deepak Karishetti <r10.deepak@gmail.com>
2019-07-25 03:19:51 -0600

    updated

--- arrays.cpp
+++ arrays.cpp
@@ -3,6 +3,12 @@
 
 using namespace std;
 
+void foo(int &x)
+{
+	x = 10;
+	cout << x << "\n";
+}
+
 int main()
 {
 	char s[5] = {'H', 'e', 'l', 'l', 'o'};
@@ -13,6 +19,19 @@ int main()
 		cout << p[i] << "\n";
 	//cout << *(p+i);
 	}
+
+	int sz;
+	cout << "enter the size: " << "\n";
+	cin >> sz;
+	int *arr = new int[sz];
+	for (int i = 0; i < sz; i++)
+	{
+		arr[i] = 0.0;
+		cout << arr[i] << " ";
+	}
+
+	int se = sizeof(arr) / sizeof(arr[0]);	
+	cout << "The size entered is: " << se << "\n" << "\n";
 
 	return 0;
 }
